1043169259980f6092ba2aa13d370c13953c5955
Winston Weinert <winston@ml1.net>
2020-10-19 09:28:24 -0500

    Fix scribblings form

--- runomatic/info.rkt
+++ runomatic/info.rkt
@@ -2,4 +2,4 @@
 
 (define racket-launcher-libraries '("bot.rkt"))
 (define racket-launcher-names '("runomatic"))
-(define scribblings '(("runomatic.scrbl") ()))
+(define scribblings '(("runomatic.scrbl" ())))
