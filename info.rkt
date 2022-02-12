84aed576c42f40661ab2f657598e42d3104a2740
Winston Weinert <winston@ml1.net>
2022-02-11 20:06:58 -0600

    Fix dependency issue

--- info.rkt
+++ info.rkt
@@ -1,10 +1,12 @@
 #lang info
 
 (define collection 'multi)
+(define version "1.1")
 
 (define deps
   (list "base"
         "html-parsing"
         "gregor"
         "request"
-        "sxml"))
+        "sxml"
+        "racket-doc"))
